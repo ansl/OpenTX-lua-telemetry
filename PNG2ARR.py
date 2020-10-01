fe147625cc21dc4c2c35fa8dbd8bbe3baaae57ec
Sanz Luengo <luengoa@amazon.es>
2020-10-01 13:12:00 +0200

    DRONE RGB mattrix

--- PNG2ARR.py
+++ PNG2ARR.py
@@ -4,13 +4,15 @@ import numpy as np
 from PIL import Image
 
 #img = Image.open("C:/Users/luengoa/Downloads/windsock.png")
-img = Image.open("drone 24.png").rotate(20)
+img = Image.open("drone 24.png").rotate(0)
 bit = np.asarray(img)
-print (bit[:,:,3])
+print (bit[:,:,0])
 
 
 
-#np.savetxt("BITMAP.txt",rawpb,fmt="%s")
-
+np.savetxt("BITMAP_R.txt",(bit[:,:,0]),fmt="%i",delimiter=',')
+np.savetxt("BITMAP_G.txt",(bit[:,:,1]),fmt="%i",delimiter=',')
+np.savetxt("BITMAP_B.txt",(bit[:,:,2]),fmt="%i",delimiter=',')
+np.savetxt("BITMAP_A.txt",(bit[:,:,3]),fmt="%i",delimiter=',')
 
 img.show()  # invoke image viewer for debugging
